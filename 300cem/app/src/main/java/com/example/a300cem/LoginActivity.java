c266577c109209fe30446785d14bffddfa7c70b8
harrychor <58283630+harrychor@users.noreply.github.com>
2020-05-02 17:48:22 +0800

    fingerprint update

---
+++ 300cem/app/src/main/java/com/example/a300cem/LoginActivity.java
@@ -0,0 +1,251 @@
+package com.example.a300cem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricPrompt;
+import androidx.fragment.app.FragmentActivity;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+
+public class LoginActivity extends AppCompatActivity {
+    FirebaseAuth auth;
+    FirebaseAuth.AuthStateListener authListener;
+    private String userUID;
+    private MutableLiveData<LoginFormState> loginFormState = new MutableLiveData<>();
+    LiveData<LoginFormState> getLoginFormState() {
+        return loginFormState;
+    }
+    private static final String TAG = LoginActivity.class.getName();
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(authListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        auth.removeAuthStateListener(authListener);
+    }
+
+    public void login(View v){
+        String email = ((EditText)findViewById(R.id.email))
+                .getText().toString();
+        String password = ((EditText)findViewById(R.id.password))
+                .getText().toString();
+        Log.d("AUTH", email+"/"+password);
+
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (!task.isSuccessful()){
+                            Log.d("onComplete", String.valueOf(R.string.login_failed));
+                            messageloginfail();
+                        } else {
+                            messageloginsuccess();
+                            Intent intent = new Intent();
+                            intent.setClass(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    }
+                });
+    }
+
+    private void messageloginsuccess() {
+        new AlertDialog.Builder(LoginActivity.this)
+                .setTitle(R.string.login_successful)
+                .setPositiveButton(R.string.cancel, null)
+                .show();
+
+    }
+
+    private void messageloginfail() {
+        new AlertDialog.Builder(LoginActivity.this)
+                .setTitle(R.string.login_failed)
+                .setMessage(R.string.login_fail_massage)
+                .setPositiveButton(R.string.cancel, null)
+                .show();
+    }
+    // username check
+    public boolean isUserNameValid(String username) {
+        if (username == null) {
+            return false;
+        }
+        if (username.contains("@")) {
+            return Patterns.EMAIL_ADDRESS.matcher(username).matches();
+        } else {
+            return !username.trim().isEmpty();
+        }
+    }
+
+    // password check
+    public boolean isPasswordValid(String password) {
+        if(password != null && password.trim().length() > 5) {
+            return true;
+        }else{
+        return false;
+        }
+    }
+
+    // check all
+    public boolean isloginValid(String username,String password){
+        if(isUserNameValid(username)==isPasswordValid(password)==true){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public void loginDataChanged(String username, String password) {
+        if (!isUserNameValid(username)) {
+            loginFormState.setValue(new LoginFormState(R.string.invalid_username, null));
+        } else if (!isPasswordValid(password)) {
+            loginFormState.setValue(new LoginFormState(null, R.string.invalid_password));
+        } else {
+            loginFormState.setValue(new LoginFormState(true));
+        }
+    }
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        auth = FirebaseAuth.getInstance();
+        final EditText checkusername = findViewById(R.id.email);
+        final EditText checkpassword = findViewById(R.id.password);
+        final Button loginButton = findViewById(R.id.loginbutton);
+        final ImageButton AuthenticationButton = findViewById(R.id.FingerAuthentication);
+        getLoginFormState().observe(this, new Observer<LoginFormState>() {
+            @Override
+            public void onChanged(LoginFormState loginFormState) {
+                if (loginFormState == null) {
+                    return;
+                }
+                loginButton.setEnabled(loginFormState.isDataValid());
+                if (loginFormState.getUsernameError() != null) {
+                    checkusername.setError(getString(loginFormState.getUsernameError()));
+                }
+                if (loginFormState.getPasswordError() != null) {
+                    checkpassword.setError(getString(loginFormState.getPasswordError()));
+                }
+            }
+        });
+
+        TextWatcher afterTextChangedListener = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                loginDataChanged(checkusername.getText().toString(),checkpassword.getText().toString());
+                loginButton.setEnabled(isloginValid(checkusername.getText().toString(),checkpassword.getText().toString()));
+            }
+        };
+        checkusername.addTextChangedListener(afterTextChangedListener);
+        checkpassword.addTextChangedListener(afterTextChangedListener);
+
+
+        //Assign an onClickListener to the app’s “Authentication” button//
+
+
+//Create a thread pool with a single thread//
+        Executor newExecutor = Executors.newSingleThreadExecutor();
+        FragmentActivity activity = this;
+
+//Start listening for authentication events//
+
+            final BiometricPrompt myBiometricPrompt = new BiometricPrompt(activity, newExecutor, new BiometricPrompt.AuthenticationCallback() {
+                @Override
+//onAuthenticationError is called when a fatal error occurrs//
+                public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                    super.onAuthenticationError(errorCode, errString);
+                    if (errorCode == BiometricPrompt.ERROR_NEGATIVE_BUTTON) {
+                    } else {
+                        Log.d(TAG, "An unrecoverable error occurred");
+                    }
+                }
+//onAuthenticationSucceeded is called when a fingerprint is matched successfully//
+                @Override
+                public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                    super.onAuthenticationSucceeded(result);
+                    Log.d(TAG, "Fingerprint recognised successfully");
+                }
+//onAuthenticationFailed is called when the fingerprint doesn’t match//
+                @Override
+                public void onAuthenticationFailed() {
+                    super.onAuthenticationFailed();
+//Print a message to Logcat//
+                    Log.d(TAG, "Fingerprint not recognised");
+                }
+            });
+//Create the BiometricPrompt instance//
+            final BiometricPrompt.PromptInfo promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                    .setTitle("Login with your Fingerprint")
+                    .setNegativeButtonText("Cancel")
+                    .build();
+//Assign an onClickListener to the app’s “Authentication” button//
+            AuthenticationButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    myBiometricPrompt.authenticate(promptInfo);
+
+
+                }
+            });
+
+
+
+
+
+        authListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(
+                    @NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Log.d("onAuthStateChanged", "登入:" +
+                            user.getUid());
+                    userUID = user.getUid();
+                } else {
+                    Log.d("onAuthStateChanged", "已登出");
+                }
+            }
+        };
+
+
+    }
+}
